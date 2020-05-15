9e364996fd4e3e831ba45e4be80137e53353a746
Niharika Gupta <54269414+LordOfDexterity@users.noreply.github.com>
2020-05-15 18:07:11 +0530

    updated code

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
 import smtplib, ssl
+import pandas as pd
+import xlrd 
 
-port = 465
+port = 465 #gmail
+
 sender = 'sender@gmail.com' #sender's address
 password = 'pwd' #password
-recieve = ['recievermail@something.com'] #reciver's mail
+
+loc = ("mail_list.xlsx") 
+  
+wb = xlrd.open_workbook(loc) 
+sheet = wb.sheet_by_index(0) 
+sheet.cell_value(0, 0) 
+
 msg = """\
-Subject: Python Mail 
-This is an email from python, wassup??
+Subject: Python Mailer
+Hey this is a mail sent from the python mail bot. 
 """
 context = ssl.create_default_context()
 
 print("Starting to send")
+
 with smtplib.SMTP_SSL("smtp.gmail.com",port, context=context ) as server:
     server.login(sender, password)
-    server.sendmail(sender, recieve, msg)
+    #for j in range(1,11):  #this loop is for spamming 10 times
+    for i in range(sheet.nrows): 
+        server.sendmail(sender, sheet.cell_value(i, 0), msg)
 print("sent")
